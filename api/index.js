8ac7bca94aa3900e93127396ab704a95d73386a9
coderRahul212000 <rahulrajput211998@gmail.com>
2025-07-29 20:46:54 +0530

    final changes 2

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const ejs = require("ejs");
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "../views"));
 
 
 app.get("/", (req,res)=>{
