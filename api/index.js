526904eba1a9c91babb5fe448df0d0eefd7e5f79
coderRahul212000 <rahulrajput211998@gmail.com>
2025-07-29 20:39:32 +0530

    my last deployment

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const ejs = require("ejs");
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.set("views", path.join(__dirname, "views"));
+
+
+app.get("/", (req,res)=>{
+    res.render("home");
+})
+
+modules.exports = app;
+
